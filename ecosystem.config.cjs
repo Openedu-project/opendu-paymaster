2c2ac2bdd21a66c9be2a153a099c3143268d100b
0xboji <hoangpichgoodkid@gmail.com>
2025-05-22 16:47:09 +0700

    docs: update deploy cmd

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'openedu-paymaster',
+      script: './dist/index.js',
+      instances: 2,
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8016
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8016
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true
+    },
+  ],
+}; 
